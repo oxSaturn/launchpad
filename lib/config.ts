5f861919bb931c3787e1837ec4546d5c7f6f72b5
duncan4123 <dmgwizz@gmail.com>
2023-05-12 12:54:24 +1000

    add goerli (not arb goerli) and add contract

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,8 @@
-import { arbitrum, canto, arbitrumGoerli } from "wagmi/chains";
+import { arbitrum, canto, arbitrumGoerli, goerli } from "wagmi/chains";
 
 export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // TODO placeholders
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
+  [goerli.id]: "0xf4E8C58FFd75279C73A4b92438e71eB2b7Ddc81f"
 } as const;
