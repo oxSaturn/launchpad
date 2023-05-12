ceb159ccffb727406f9d1001f19237aa388fb077
duncan4123 <dmgwizz@gmail.com>
2023-05-12 21:56:29 +1000

    latest contract with dmt

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // TODO placeholders
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
-  [goerli.id]: "0xAfACc2928DF00CBE05B0bC41DAb5FfD792A6A04c", // use epoch time not block
+  [goerli.id]: "0x767D9ad09e4E148e0Ae23Ea1F2dB04e5F0Cd2EdA", // use epoch time not block
 } as const;
