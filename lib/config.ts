80d2f9dd9cf68c55e30d528d7b5fa567473e96ef
duncan4123 <dmgwizz@gmail.com>
2023-05-12 17:04:57 +1000

    use new test project token

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // TODO placeholders
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
-  [goerli.id]: "0x13661E41f6AFF14DE677bbD692601bE809a14F76", // use epoch time not block
+  [goerli.id]: "0x29f9BF5e35Eb456b8b39580e66Aea73D5a988b04", // use epoch time not block
 } as const;
