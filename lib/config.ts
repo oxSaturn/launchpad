256819dde311a31bd6c5a76901af88d3045c5243
duncan4123 <dmgwizz@gmail.com>
2023-05-12 14:46:05 +1000

    motto contract

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // TODO placeholders
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
-  [goerli.id]: "0x4DdcF5BD9852caBb28f0bEaf30196715eDa18D6d"
+  [goerli.id]: "0x4DdcF5BD9852caBb28f0bEaf30196715eDa18D6d" // motto contract
 } as const;
