c664757dec429036bdf1da1c1bf565c168972fe6
duncan4123 <dmgwizz@gmail.com>
2023-05-12 21:50:25 +1000

    new contract

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // TODO placeholders
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d",
-  [goerli.id]: "0xD59CC0112A63A0b4050c819908d07dd30e741A7E", // use epoch time not block
+  [goerli.id]: "0xAfACc2928DF00CBE05B0bC41DAb5FfD792A6A04c", // use epoch time not block
 } as const;
