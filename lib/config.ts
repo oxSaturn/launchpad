73c39b80588afd83ed410be9615b0f840f3cfc28
duncan4123 <dmgwizz@gmail.com>
2023-05-16 10:57:09 +1000

    rt

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const fairAuctionContractAddresses = {
   [arbitrum.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // placeholder
   [arbitrumGoerli.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // placeholder
   [canto.id]: "0x0b776552c1aef1dc33005dd25acda22493b6615d", // placeholder
-  [goerli.id]: "0x5Bd42D24CbBb35aB101cc68C027C7fB663086C88", // TODO deployment chore
+  [goerli.id]: "0xe4e39196c8c1998f63db6c6a2a54521b21240614", // TODO deployment chore
 } as const;
