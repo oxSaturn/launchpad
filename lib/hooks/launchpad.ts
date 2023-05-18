b00053aebf63c13933e04486fff06f0878334378
t0rbik <liker1011@gmail.com>
2023-05-18 21:49:44 +0700

    Revert "check origin"
    This reverts commit fbe309886fcafb6e3643c558acb26d3d6cd1c2c5.

--- lib/hooks/launchpad.ts
+++ lib/hooks/launchpad.ts
@@ -15,6 +15,8 @@ import {
   useFairAuctionTotalRaised,
 } from "../generated/wagmiGen";
 
+import { dmt_whitelist } from "../../dmt_whitelist";
+
 const SECOND = 1_000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
@@ -153,16 +155,12 @@ export function useTimer() {
       };
 }
 
-const getIsWhitelisted = async (address: string | undefined) => {
-  const whitelist = await fetch(
-    "https://anythingyouwantintheworld.com/DMT_whitelistsale.json"
-  );
-  const whitelistJson = (await whitelist.json()) as string[];
-
-  return whitelistJson.some(
+const getIsWhitelisted = (address: string | undefined) => {
+  const isWhitelisted = dmt_whitelist.some(
     (whitelistedAddress) =>
       whitelistedAddress.toLowerCase() === address?.toLowerCase()
   );
+  return isWhitelisted;
 };
 
 export function useIsWhitelisted() {
